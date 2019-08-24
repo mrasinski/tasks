0cacacf318ec289faf4842a5ae39d28be59aaae4
mrasinski <ariel_6@wp.pl>
2019-08-24 10:10:35 +0200

    button

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -16,6 +16,8 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "https://trello.com/raszi88/boards");
+        context.setVariable("button","Visit website");
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
